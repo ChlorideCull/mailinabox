a735bfaa82f938783d99c674b86de4a67b89a7bc
Joshua Tauberer <jt@occams.info>
2015-10-10 22:03:55 +0000

    provision and install free SSL certificates from Let's Encrypt

--- management/daily_tasks.sh
+++ management/daily_tasks.sh
@@ -4,5 +4,8 @@
 # Take a backup.
 management/backup.py
 
+# Provision any new certificates for new domains or domains with expiring certificates.
+management/ssl_certificates.py --headless
+
 # Run status checks and email the administrator if anything changed.
 management/status_checks.py --show-changes --smtp
